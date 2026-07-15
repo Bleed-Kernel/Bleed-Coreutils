9e384cdc9df11b287a5280690eeddba271d1a0c2
mellurboo <isabella@chromebook.bleedkernel.com>
2026-07-15 14:56:11 +0100

    added open type specificers

--- write/src/write.c
+++ write/src/write.c
@@ -8,22 +8,50 @@
 #include <syscalls/close.h>
 #include <syscalls/exit.h>
 
+int get_permission(char perm[8]){
+    if (strcmp(perm, "rw") == 0){
+        return O_RDONLY;
+    }else if (strcmp(perm, "wr") == 0){
+        return O_WRONLY;
+    }else if (strcmp(perm, "rdwr") == 0){
+        return O_RDWR;
+    }else if (strcmp(perm, "mode") == 0){
+        return O_MODE;
+    }else if (strcmp(perm, "creat") == 0){
+        return O_CREAT;
+    }else if (strcmp(perm, "trunc") == 0){
+        return O_TRUNC;
+    }else if (strcmp(perm, "append") == 0){
+        return O_APPEND;
+    }else{
+        return -1;
+    }
+
+    // holy this is bad, oh well.
+}
+
 //quick and dirty program for writing directly to file descriptors or devices.
-// theres also like no point freeing any of these heap allocations the program lives
-// for like a few milliseconds, the kernel will handle it (tm).
 int main(int argc, const char** argv){
-    if (argc != 3){
-        printf("Bad Argument Count (expected write <value> <destination>)\n");
+    if (argc != 4){
+        printf("Bad Argument Count (expected write <value> <destination> <permission[rd, wr, rdwr, mode, creat, trunc, append]>)\n");
         _exit(1);
     }
     
+    char permission[8];
+    strncpy(permission, argv[3], sizeof(permission));
+
+    int permission_level = get_permission(permission);
+    if (permission_level == -1){
+        printf("Bad Permission Level, either rd, wr, rdwr, mode, creat, trunc or append\n");
+    }
+
     char filepath[4096];
     strncpy(filepath, argv[2], sizeof(filepath));
 
     char value[4096];
     strncpy(value, argv[1], sizeof(value));
-
-    int fd = (int)_open(filepath, O_RDWR);
+    
+    int fd = (int)_open(filepath, permission_level);
     if (fd < 0){
         printf("File or device %s not found!", filepath);
         _exit(1);
