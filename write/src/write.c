23ef42e518181e73842aba91bb3e5547d62894a0
mellurboo <isabella@chromebook.bleedkernel.com>
2026-07-15 14:06:35 +0100

    hmm

--- write/src/write.c
+++ write/src/write.c
@@ -2,12 +2,15 @@
 #include <stdint.h>
 #include <string.h>
 #include <fs/file.h>
+#include <stdlib.h>
 #include <syscalls/write.h>
 #include <syscalls/open.h>
 #include <syscalls/close.h>
 #include <syscalls/exit.h>
 
 //quick and dirty program for writing directly to file descriptors or devices.
+// theres also like no point freeing any of these heap allocations the program lives
+// for like a few milliseconds, the kernel will handle it (tm).
 int main(int argc, const char** argv){
     if (argc != 3){
         printf("Bad Argument Count (expected write <value> <destination>)\n");
@@ -15,13 +18,17 @@ int main(int argc, const char** argv){
     }
     
     char filepath[4096];
-    strncpy(filepath, argv[1], 4096);
+    strncpy(filepath, argv[2], 4096);
+
+    char* value = malloc(sizeof(argv[1]));
+    memcpy(value, argv[1], sizeof(argv[1]));
 
     int fd = (int)_open(filepath, O_RDWR);
     if (fd < 0){
         printf("File or device %s not found!", filepath);
+        _exit(1);
     }
 
-    _write(fd, filepath, sizeof(argv[1]));
+    _write(fd, value, sizeof(value));
     _close(fd);
 }
